0969e194f7d9f24a0ccddb96c05ee9deb06cb6b6
VRichh123 <139034817+VRichh123@users.noreply.github.com>
2025-06-14 14:17:27 +0530

    Add files via upload

---
+++ Assignment_2.py
@@ -0,0 +1,30 @@
+# -------------------------TASK 1 - Number is even or Odd----------------------------------
+'''
+
+Numbe_r = int(input("Enter a number : "))
+
+if Numbe_r % 2 == 0:
+    print(f"{Numbe_r} is an even number")
+
+else:
+    print(f"{Numbe_r} is an odd number")
+
+'''
+
+
+# -------------------------TASK 2 - Sum of Integers from 1 to 50----------------------------
+
+'''
+
+sum_total = 0
+
+i = 0
+
+while i <= 50:
+
+    sum_total += i
+    i = i + 1
+
+print(sum_total)
+
+'''
