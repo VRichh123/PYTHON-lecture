8f83da6949fd3be189552cb36fa9c9be231ae1e9
VRichh123 <139034817+VRichh123@users.noreply.github.com>
2025-06-14 14:00:22 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,35 @@
+# ----------------------------TASK 1 - MATHS OPERATIONS----------------------------------
+
+num_1 = int(input("Enter first number : "))
+num_2 = int(input("Enter second number : "))
+
+Add_num = num_1 + num_2
+Sub_num = num_1 - num_2
+Mul_num = num_1 * num_2
+Div_num = float(num_1) / float(num_2)
+
+
+print("Addition = ", Add_num)
+print("Substraction = ", Sub_num)
+print("Multiplication = ", Mul_num)
+print("Division = ", Div_num)
+
+
+
+
+
+# -------------------------TASK 2 - Personalised Greeting----------------------------------
+
+f_name = input("Enter your first name : ")
+l_name = input("Enter your last name : ")
+
+print(f"Hello {f_name} {l_name}, Welcome to the python program.")
+
+
+
+
+
+
+
+
+
